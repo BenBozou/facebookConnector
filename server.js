9fca33b9bf833e075e5b067ce723c4eb8d6fd1c4
Benjamin Bozou <bbozou@bbozou-ltm2.internal.salesforce.com>
2017-06-10 23:35:37 +0300

    salesforce service

--- server.js
+++ server.js
@@ -60,43 +60,9 @@ app.get('/webhookSalesforce', (req, res) => {
 });
 
 
-function DumpObject(obj)
-{
-  var od = new Object;
-  var result = "";
-  var len = 0;
-
-  for (var property in obj)
-  {
-    var value = obj[property];
-    if (typeof value == 'string')
-      value = "'" + value + "'";
-    else if (typeof value == 'object')
-    {
-      if (value instanceof Array)
-      {
-        value = "[ " + value + " ]";
-      }
-      else
-      {
-        var ood = DumpObject(value);
-        value = "{ " + ood.dump + " }";
-      }
-    }
-    result += "'" + property + "' : " + value + ", ";
-    len++;
-  }
-  od.dump = result.replace(/, $/, "");
-  od.len = len;
-
-  return od;
-}
-
 app.post('/webhookSalesforce', (req, res) => {
     let test = req.body.test;
-    console.log('Salesforce reaches heroku POST with message: ' + req.body);
-    var ob = DumpObject(req.body);
-    ob.dump();
+    console.log('Salesforce reaches heroku POST with message: ' + JSON.stringify(req.body));
     console.log('Salesforce reaches heroku POST with message: ' + test);
 
     res.sendStatus(200);
