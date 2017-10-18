a0c8aa8babaf47ce2a99b52ff7215f5dd2469928
Benjamin Bozou <bbozou@bbozou-ltm2.internal.salesforce.com>
2017-10-18 16:04:59 +0300

    test

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ let startLongPolling = (affinityToken, sessionKey, session, lastSentRequest) => 
     function callback(error, response, body) {
         if (!error && (response.statusCode == 200 || response.statusCode == 204)) {
             console.log('result: ' + body);
-            if (!(body == '')) {
+            if (!(body == '' || body == 'OK')) {
                 var bodyJson = JSON.parse(body);
                 console.log('recieved ' + bodyJson.messages.length + ' messages');
                 console.log(bodyJson.messages);
